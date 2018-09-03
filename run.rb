82b210cae3e4a967731ec56764dd7c473948ed7a
Viktharien Volander <30982528+viktharien@users.noreply.github.com>
2018-09-02 21:56:27 -0500

    Update run.rb

--- run.rb
+++ run.rb
@@ -1,42 +1,44 @@
 class Gematria
-  attr_accessor :vowel_sum
 
   def create_alphabet
     arr = ('a'..'z').to_a
+    nums = (1..9).to_a
     count = 1; vals = {}
     arr.each { |i| vals[i] = count; count < 9 ? count += 1 : count = 1 }
+    nums.each { |i| vals[i.to_s] = i }
     vals
   end
 
   def translate(input)
     temp = input
     input = input.strip.downcase.split('')
-    vowels = ["a","e","i","o","u"]; @vowel_sum = []
-    input.each { |i| @vowel_sum << alphabet[i] if vowels.include?(i) }
-    puts @vowel_sum
     alphabet = create_alphabet; translation = []
+    vowels = ["a","e","i","o","u"]; vowel_sum = []; cons_sum = []
+    input.each { |i| vowel_sum << alphabet[i] if vowels.include?(i) }
+    input.each { |i| cons_sum << alphabet[i] if !vowels.include?(i) }
     input.each { |i| translation << (alphabet[i] || 0) }
     sequence = translation.join
     p "#{temp}:: Sequence: #{sequence} Sum #{translation.reduce(&:+)}"
-    sum(translation.join)
+    puts "Your numbers are: #{sum(vowel_sum.join + cons_sum.join, "Total")} #{sum(vowel_sum.join, "Vowels")} #{sum(cons_sum.join, "Consonants")}"
+    
   end
 
-  def sum(translation)
+  def sum(translation, desc)
     if translation == 11 || translation == 22 || translation == 33
-      output translation
+      output(translation)
     else
       val = translation.to_s.split('')
       if val.length > 1
         val = val.reduce(&:+).each_char.map(&:to_i).reduce(&:+)
-        sum val
+        sum val, desc
       else
-        output translation
+        output(translation)
       end
     end
   end
 
   def output(translation)
-    p "Number: #{translation}"
+    translation
   end
 end
 
